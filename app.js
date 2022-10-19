70dd0d2ad33988cd93460fa19cf83708cf505fbc
iamsom <78479619+iamsom7@users.noreply.github.com>
2022-10-19 13:54:45 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const spawn = require('child_process').spawn;
+
+let FilePath = 'VIDEO PATH HERE';
+let streamKey = 'STREAM KEY HERE';
+
+let ffmpeg = spawn('ffmpeg', ['-re', '-stream_loop', '-1', '-i', `${ FilePath }`, '-c:v', 'libx264', '-preset', 'veryfast',
+                   '-framerate', '60', '-maxrate', '3000k', '-bufsize', '6000k', '-pix_fmt', 'yuv420p', '-g', '50', '-c:a', 'aac', '-b:a', '128k',
+                   '-ac', '2', '-ar', '44100', '-f', 'flv', `${ streamKey }`]);
+ffmpeg.on('exit', (statusCode) => {
+  if (statusCode === 0) {
+     console.log('Stream Completed')
+  }
+})
+
+ffmpeg
+  .stderr
+  .on('data', (task) => {
+    console.log('Task:', new String(task))
+  })
